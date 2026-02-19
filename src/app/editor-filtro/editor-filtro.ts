99fa4ab03552948919adddf667513b642dae3575
apoyerko03-hash <apoyerko03@gmail.com>
2026-02-18 20:25:58 -0400

    comit 1

---
+++ src/app/editor-filtro/editor-filtro.ts
@@ -0,0 +1,23 @@
+import { Component , signal , computed} from '@angular/core';
+
+@Component({
+  selector: 'app-editor-filtro',
+  standalone: true,
+  templateUrl: './editor-filtro.html',
+  styleUrl: './editor-filtro.scss',
+})
+export class EditorFiltroComponent {
+brillo= signal(100);
+contraste = signal(100);
+blur = signal(0);   //pixeles
+filtroScss = computed(()=>{
+  return `brightness(${this.brillo()}%) contrast(${this.contraste()}%) blur(${this.blur()}px)`;
+})
+actualizar = (prop: string, evento: Event){
+const valor = (evento.target as HTMLInputElement).value; //recicir una propiedad y un evento
+if (prop == 'brillo') this.brillo.set(+valor);
+if (prop == 'contraste') this.contraste.set(+valor);
+if (prop == 'blur') this.blur.set(+valor); //valores obtenidos incrementando
+}
+}
+
